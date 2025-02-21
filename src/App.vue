d34b5daa681c9c8f9473a99dfa9e9662f7604c0e
Polina Severina <polinaseverina@Polinas-MacBook-Air.local>
2025-02-21 22:09:26 +0100

    [SCOUT]: cleaned up the code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/details">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
-  <!-- <h1 class="title">{{ $route.name }}</h1> -->
   <RouterView />
 </template>
